c316cce46217ebe9a5ae1dee701e05ec7708e1b3
sinisuba <68671543+sinisuba@users.noreply.github.com>
2021-07-14 15:43:11 +0200

    Solved | sumZero
    Find N Unique Integers Sum up to Zero

---
+++ sumZero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        std::vector<int> ans;
+        
+        if (n % 2)
+            ans.push_back(0);
+        
+        for (int i = 1; i <= n / 2; ++i)
+        {
+            ans.push_back(i);
+            ans.push_back(i * -1);
+        }
+        
+        return ans;
+    }
+};
