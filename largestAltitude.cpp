97bb36e2712ee72120e132c2bcf481d3b22e3dd3
sinisuba <68671543+sinisuba@users.noreply.github.com>
2021-07-14 15:49:58 +0200

    Solved | largestAltitude
    Find the Highest Altitude

---
+++ largestAltitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr = 0, ans = 0;
+        
+        for (int i = 0; i < gain.size(); ++i)
+        {
+            curr += gain[i];
+            ans = max(curr, ans);
+        }
+        
+        return ans;
+    }
+};
